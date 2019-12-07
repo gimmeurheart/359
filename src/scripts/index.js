259a41f28fd95656883e535c9e7d5564a2c7ae5f
gimmeurheart <frelight@bk.ru>
2019-12-07 19:45:58 +0300

    v2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import initAnother from './parts/another';
 import initSliders from './parts/sliders';
+import initForm from './parts/form';
 
 $(document).ready(() => {
   initAnother();
   initSliders();
+  initForm();
 });
